111970dea287fc08892b2cc14a44ec8aa231022a
nail-e <pavel.ay0t@gmail.com>
2023-09-16 09:10:43 +0400

    Uploaded conversion v1.0.0

--- conversion.py
+++ conversion.py
@@ -1,3 +1,9 @@
+'''
+conversion.py
+v1.0.0
+nail_
+'''
+
 class Conversion():
     def __init__(self):
         self.origin = 0
@@ -80,6 +86,112 @@ class Conversion():
                 print("Invalid input")
         print(self.truncate(self.calculation()))
 
+    def extended(self):
+        self.input = float(input("Insert input value (as decimal!) "))
+        print("1. femto (f)")
+        print("2. pico (p)")
+        print("3. nano (n)")
+        print("4. micro (u)")
+        print("5. milli (m)")
+        print("6. centi (c)")
+        print("7. deci (d)")
+        print("8. deca (da)")
+        print("9. hecto (h)")
+        print("10. kilo (k)")
+        print("11. Mega (M)")
+        print("12. Giga (G)")
+        print("13. Tera (T)")
+        print("14. Peta (P)")
+        print("15. (-) No prefix")
+        
+        user_Input = input("Choose original unit ")
+        while self.origin == 0:
+            if user_Input == "1":
+                self.origin = self.conversion_values["f"]
+            elif user_Input == "2":
+                self.origin = self.conversion_values["p"]
+            elif user_Input == "3":
+                self.origin = self.conversion_values["n"]
+            elif user_Input == "4":
+                self.origin = self.conversion_values["u"]
+            elif user_Input == "5":
+                self.origin = self.conversion_values["m"]
+            elif user_Input == "6":
+                self.origin = self.conversion_values["c"]
+            elif user_Input == "7":
+                self.origin = self.conversion_values["d"]
+            elif user_Input == "8":
+                self.origin = self.conversion_values["da"]
+            elif user_Input == "9":
+                self.origin = self.conversion_values["h"]
+            elif user_Input == "10":
+                self.origin = self.conversion_values["k"]
+            elif user_Input == "11":
+                self.origin = self.conversion_values["M"]
+            elif user_Input == "12":
+                self.origin = self.conversion_values["G"]
+            elif user_Input == "13":
+                self.origin = self.conversion_values["T"]
+            elif user_Input == "14":
+                self.origin = self.conversion_values["P"]
+            elif user_Input == "15":
+                self.origin = self.conversion_values["-"]
+            else:
+                print("Invalid input")
+
+        print("1. femto (f)")
+        print("2. pico (p)")
+        print("3. nano (n)")
+        print("4. micro (u)")
+        print("5. milli (m)")
+        print("6. centi (c)")
+        print("7. deci (d)")
+        print("8. deca (da)")
+        print("9. hecto (h)")
+        print("10. kilo (k)")
+        print("11. Mega (M)")
+        print("12. Giga (G)")
+        print("13. Tera (T)")
+        print("14. Peta (P)")
+        print("15. (-) No prefix")
+        
+        user_Input = input("Choose final unit ")
+        while self.final == 0:
+            if user_Input == "1":
+                self.final = self.conversion_values["f"]
+            elif user_Input == "2":
+                self.final = self.conversion_values["p"]
+            elif user_Input == "3":
+                self.final = self.conversion_values["n"]
+            elif user_Input == "4":
+                self.final = self.conversion_values["u"]
+            elif user_Input == "5":
+                self.final = self.conversion_values["m"]
+            elif user_Input == "6":
+                self.final = self.conversion_values["c"]
+            elif user_Input == "7":
+                self.final = self.conversion_values["d"]
+            elif user_Input == "8":
+                self.final = self.conversion_values["da"]
+            elif user_Input == "9":
+                self.final = self.conversion_values["h"]
+            elif user_Input == "10":
+                self.final = self.conversion_values["k"]
+            elif user_Input == "11":
+                self.final = self.conversion_values["M"]
+            elif user_Input == "12":
+                self.final = self.conversion_values["G"]
+            elif user_Input == "13":
+                self.final = self.conversion_values["T"]
+            elif user_Input == "14":
+                self.final = self.conversion_values["P"]
+            elif user_Input == "15":
+                self.final = self.conversion_values["-"]
+            else:
+                print("Invalid input")
+
+        print(self.truncate(self.calculation()))
+   
     def main(self):
         user_Input = "0"
         while user_Input == "0":
